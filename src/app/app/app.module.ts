032c073a9c2097868e443dacf703991c0c9ff151
hari <91301971+haran2996@users.noreply.github.com>
2024-02-24 08:10:43 +0000

    Adding skeletons for dashboard, login and signup component

--- src/app/app/app.module.ts
+++ src/app/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
+import { DashboardComponent } from "../dashboard/dashboard.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DashboardComponent],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
